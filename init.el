d54c58a0e2c3ada0512d50ac1758c0a3d2beecb4
dhawangg@outlook.com <dhawangg@outlook.com>
2022-05-18 09:06:56 -0700

    commit: 05182022_0906

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 (tool-bar-mode -1)
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
+(add-to-list 'load-path "~/.emacs.d/lisp/expand-region")
 
 (require 'which-key)
 (which-key-mode 1)
@@ -19,6 +20,12 @@
       recentf-max-menu-items 15)
 (recentf-mode)
 
+(require 'expand-region)
+(global-set-key (kbd "<f7>") 'er/expand-region)
+
+;; https://pragmaticemacs.wordpress.com/2015/05/11/move-to-startend-of-line-or-sentence/
+(setq sentence-end-double-space nil)
+
 ;; keybindings
 (define-key xah-fly-command-map (kbd "<kp-8>") 'org-previous-item)
 (define-key xah-fly-command-map (kbd "<kp-5>") 'org-next-item)
@@ -26,14 +33,25 @@
 (define-key xah-fly-command-map (kbd "<kp-4>") 'org-next-visible-heading)
 (define-key xah-fly-insert-map (kbd "\\") 'xah-fly-command-mode-activate)
 (define-key xah-fly-command-map (kbd "z") 'make-frame-command)
-(define-key xah-fly-command-map (kbd "<kp-2>") 'delete-frame)
+(define-key xah-fly-command-map (kbd "<kp-0>") 'delete-frame)
+
+(define-key xah-fly-command-map (kbd "<kp-3>") 'xah-pop-local-mark-ring)
 
 (define-key key-translation-map (kbd "<apps>") (kbd "<menu>"))
 (define-key xah-fly-command-map (kbd "<menu>") 'org-edit-src-exit)
+
 (define-key xah-fly-command-map (kbd "<kp-1>") 'xah-new-empty-buffer)
+(define-key xah-fly-command-map (kbd "<kp-2>") 'xah-cycle-hyphen-lowline-space)
 
 (define-key xah-fly-command-map (kbd "<kp-9>") 'org-backward-heading-same-level)
 (define-key xah-fly-command-map (kbd "<kp-6>") 'org-forward-heading-same-level)
+
+(global-set-key (kbd "<f5>") 'backward-sentence)
+(global-set-key (kbd "<f6>") 'forward-sentence)
+
+(setq visible-bell 1)
+
+;; (define-key xah-fly-command-map (kbd "<f8>") 'backward-sentence)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -43,13 +61,14 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(blink-cursor-mode nil)
- '(custom-enabled-themes '(wombat))
+ '(custom-enabled-themes '(leuven))
  '(visible-cursor nil)
- '(xah-fly-keys nil))
+ '(xah-fly-keys t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil
+)
