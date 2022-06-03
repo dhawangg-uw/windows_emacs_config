843d16ee9d9b9d160f04f64a3bcb347e748bfd3a
dhawangg@outlook.com <dhawangg@outlook.com>
2022-06-03 14:52:19 -0700

    commit: 06032022_1452

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
-; Install msys64 and install aspell tools inside
-; Steps of installation: https://www.msys2.org/
+  ; Install msys64 and install aspell tools inside
+  ; Steps of installation: https://www.msys2.org/
 (setq-default ispell-program-name "C:/msys64/mingw64/bin/aspell.exe")
 (tool-bar-mode -1)
 
@@ -51,149 +51,149 @@
 ;; https://emacs.stackexchange.com/questions/12487/when should i use with eval after load in my configuration files
 (require 'org-install)
 (with-eval-after-load 'org
-(defun zp/org-fold (&optional keep-position)
-  (let ((indirectp (not (buffer-file-name)))
-        (org-startup-folded 'overview))
-    ;; Fold drawers
-    (org-set-startup-visibility)
-    ;; Fold trees
-    (org-overview)
-    (unless keep-position
-      (goto-char (point-min)))
-    (recenter)
-    (save-excursion
-      (goto-char (point-min))
-      (org-show-entry)
-      (when (org-at-heading-p)
-        (org-show-children)))))
+  (defun zp/org-fold (&optional keep-position)
+    (let ((indirectp (not (buffer-file-name)))
+          (org-startup-folded 'overview))
+      ;; Fold drawers
+      (org-set-startup-visibility)
+      ;; Fold trees
+      (org-overview)
+      (unless keep-position
+	(goto-char (point-min)))
+      (recenter)
+      (save-excursion
+	(goto-char (point-min))
+	(org-show-entry)
+	(when (org-at-heading-p)
+          (org-show-children)))))
 
-(defun zp/org-narrow-previous-heading (arg)
-  "Move to the previously narrowed tree, and narrow the buffer to it."
-  (interactive "p")
-  (if (bound-and-true-p zp/org-narrow-previous-position)
-      (let ((pos-before zp/org-narrow-previous-position))
-        (goto-char zp/org-narrow-previous-position)
-        (org-reveal)
-        (org-cycle)
-        (org-narrow-to-subtree)
-        (setq zp/org-narrow-previous-position nil)
-        (message "Narrowing to previously narrowed tree."))
-    (message "Couldn’t find a previous position.")))
+  (defun zp/org-narrow-previous-heading (arg)
+    "Move to the previously narrowed tree, and narrow the buffer to it."
+    (interactive "p")
+    (if (bound-and-true-p zp/org-narrow-previous-position)
+	(let ((pos-before zp/org-narrow-previous-position))
+          (goto-char zp/org-narrow-previous-position)
+          (org-reveal)
+          (org-cycle)
+          (org-narrow-to-subtree)
+          (setq zp/org-narrow-previous-position nil)
+          (message "Narrowing to previously narrowed tree."))
+      (message "Couldn’t find a previous position.")))
 
-(defun zp/org-narrow-to-subtree ()
-  "Move to the next subtree at same level, and narrow the buffer to it."
-  (interactive)
-  (org-narrow-to-subtree)
-  (zp/org-fold nil)
-  (when (called-interactively-p 'any)
-    (message "Narrowing to tree at point.")
-    (run-hooks 'zp/org-after-view-change-hook)))
-
-(defun zp/org-widen ()
-  "Move to the next subtree at same level, and narrow the buffer to it."
-  (interactive)
-  (let ((pos-before (point)))
-    (setq-local zp/org-narrow-previous-position pos-before))
-  (widen)
-  (when (called-interactively-p 'any)
-    (message "Removing narrowing.")
-    (run-hooks 'zp/org-after-view-change-hook)))
-
-(defvar zp/presentation-mode nil)
-
-(defun zp/org-narrow-forwards ()
-  "Move to the next subtree at same level, and narrow the buffer to it."
-  (interactive)
-  (widen)
-  (org-forward-heading-same-level 1)
-  (org-narrow-to-subtree)
-  (unless zp/presentation-mode
-    (zp/org-fold nil))
-  (when (called-interactively-p 'any)
-    (message "Narrowing to next tree.")
-    (run-hooks 'zp/org-after-view-change-hook)))
-
-(defun zp/org-narrow-backwards ()
-  "Move to the next subtree at same level, and narrow the buffer to it."
-  (interactive)
-  (widen)
-  (org-backward-heading-same-level 1)
-  (org-narrow-to-subtree)
-  (unless zp/presentation-mode
-    (zp/org-fold nil))
-  (when (called-interactively-p 'any)
-    (message "Narrowing to previous tree.")
-    (run-hooks 'zp/org-after-view-change-hook)))
-
-(defun zp/org-narrow-up-heading (&optional arg keep-position)
-  "Move to the upper subtree, and narrow the buffer to it."
-  (interactive "p")
-  (unless (buffer-narrowed-p)
-    (user-error "No narrowing"))
-  (let ((pos-before (point)))
-    (setq-local zp/org-narrow-previous-position pos-before)
-    (widen)
-    (org-reveal)
-    (outline-up-heading 1)
+  (defun zp/org-narrow-to-subtree ()
+    "Move to the next subtree at same level, and narrow the buffer to it."
+    (interactive)
     (org-narrow-to-subtree)
-    (when (or (eq arg 4)
-              keep-position)
-      (goto-char pos-before)
-      (recenter-top-bottom))
-    (zp/org-fold (or (eq arg 4)
-                     keep-position))
-    (when arg
-      (message "Narrowing to tree above.")
-      (run-hooks 'zp/org-after-view-change-hook))))
+    (zp/org-fold nil)
+    (when (called-interactively-p 'any)
+      (message "Narrowing to tree at point.")
+      (run-hooks 'zp/org-after-view-change-hook)))
 
-(defun zp/org-narrow-up-heading-dwim (arg)
-  "Narrow to the upper subtree, and narrow the buffer to it.
+  (defun zp/org-widen ()
+    "Move to the next subtree at same level, and narrow the buffer to it."
+    (interactive)
+    (let ((pos-before (point)))
+      (setq-local zp/org-narrow-previous-position pos-before))
+    (widen)
+    (when (called-interactively-p 'any)
+      (message "Removing narrowing.")
+      (run-hooks 'zp/org-after-view-change-hook)))
+
+  (defvar zp/presentation-mode nil)
+
+  (defun zp/org-narrow-forwards ()
+    "Move to the next subtree at same level, and narrow the buffer to it."
+    (interactive)
+    (widen)
+    (org-forward-heading-same-level 1)
+    (org-narrow-to-subtree)
+    (unless zp/presentation-mode
+      (zp/org-fold nil))
+    (when (called-interactively-p 'any)
+      (message "Narrowing to next tree.")
+      (run-hooks 'zp/org-after-view-change-hook)))
+
+  (defun zp/org-narrow-backwards ()
+    "Move to the next subtree at same level, and narrow the buffer to it."
+    (interactive)
+    (widen)
+    (org-backward-heading-same-level 1)
+    (org-narrow-to-subtree)
+    (unless zp/presentation-mode
+      (zp/org-fold nil))
+    (when (called-interactively-p 'any)
+      (message "Narrowing to previous tree.")
+      (run-hooks 'zp/org-after-view-change-hook)))
+
+  (defun zp/org-narrow-up-heading (&optional arg keep-position)
+    "Move to the upper subtree, and narrow the buffer to it."
+    (interactive "p")
+    (unless (buffer-narrowed-p)
+      (user-error "No narrowing"))
+    (let ((pos-before (point)))
+      (setq-local zp/org-narrow-previous-position pos-before)
+      (widen)
+      (org-reveal)
+      (outline-up-heading 1)
+      (org-narrow-to-subtree)
+      (when (or (eq arg 4)
+		keep-position)
+	(goto-char pos-before)
+	(recenter-top-bottom))
+      (zp/org-fold (or (eq arg 4)
+                       keep-position))
+      (when arg
+	(message "Narrowing to tree above.")
+	(run-hooks 'zp/org-after-view-change-hook))))
+
+  (defun zp/org-narrow-up-heading-dwim (arg)
+    "Narrow to the upper subtree, and narrow the buffer to it.
 If the buffer is already narrowed to level-1 heading, overview
 the entire buffer."
-  (interactive "p")
-  (if (save-excursion
-        ;; Narrowed to a level-1 heading?
-        (goto-char (point-min))
-        (and (buffer-narrowed-p)
-             (equal (org-outline-level) 1)))
-      (zp/org-overview arg)
-    (zp/org-narrow-up-heading arg)))
+    (interactive "p")
+    (if (save-excursion
+          ;; Narrowed to a level-1 heading?
+          (goto-char (point-min))
+          (and (buffer-narrowed-p)
+               (equal (org-outline-level) 1)))
+	(zp/org-overview arg)
+      (zp/org-narrow-up-heading arg)))
 
-(defun zp/org-narrow-previous-heading (arg)
-  "Move to the previously narrowed tree, and narrow the buffer to it."
-  (interactive "p")
-  (if (bound-and-true-p zp/org-narrow-previous-position)
-      (let ((pos-before zp/org-narrow-previous-position))
-        (goto-char zp/org-narrow-previous-position)
-        (org-reveal)
-        (org-cycle)
-        (org-narrow-to-subtree)
-        (setq zp/org-narrow-previous-position nil)
-        (message "Narrowing to previously narrowed tree."))
-    (message "Couldn’t find a previous position.")))
+  (defun zp/org-narrow-previous-heading (arg)
+    "Move to the previously narrowed tree, and narrow the buffer to it."
+    (interactive "p")
+    (if (bound-and-true-p zp/org-narrow-previous-position)
+	(let ((pos-before zp/org-narrow-previous-position))
+          (goto-char zp/org-narrow-previous-position)
+          (org-reveal)
+          (org-cycle)
+          (org-narrow-to-subtree)
+          (setq zp/org-narrow-previous-position nil)
+          (message "Narrowing to previously narrowed tree."))
+      (message "Couldn’t find a previous position.")))
 
-;; Toggle fontifications
-(defun zp/org-toggle-emphasis-markers (&optional arg)
-  "Toggle emphasis markers."
-  (interactive "p")
-  (let ((markers org-hide-emphasis-markers))
-    (if markers
-        (setq-local org-hide-emphasis-markers nil)
-      (setq-local org-hide-emphasis-markers t))
-    (when arg
-      (font-lock-fontify-buffer))))
+  ;; Toggle fontifications
+  (defun zp/org-toggle-emphasis-markers (&optional arg)
+    "Toggle emphasis markers."
+    (interactive "p")
+    (let ((markers org-hide-emphasis-markers))
+      (if markers
+          (setq-local org-hide-emphasis-markers nil)
+	(setq-local org-hide-emphasis-markers t))
+      (when arg
+	(font-lock-fontify-buffer))))
 
 
 
   
   (define-key org-mode-map (kbd "<f2>") 'org-export-dispatch)
-  (define-key org-mode-map (kbd "<f8>") 'org-insert-structure-template)
+  (define-key org-mode-map (kbd "<kp-subtract>") 'org-insert-structure-template)
   (define-key org-mode-map (kbd "<f10>") 'org-edit-special)
   (define-key org-mode-map (kbd "<next>") 'org-next-block)
   (define-key org-mode-map (kbd "<prior>") 'org-previous-block)
 
-  (define-key org-mode-map (kbd "<f8>") 'zp/org-narrow-backwards)
-  (define-key org-mode-map (kbd "<f9>") 'zp/org-narrow-forwards)
+  (define-key org-mode-map (kbd "<f9>") 'zp/org-narrow-backwards)
+  (define-key org-mode-map (kbd "<f8>") 'zp/org-narrow-forwards)
   (define-key org-mode-map (kbd "<f12>") 'zp/org-narrow-up-heading))
 
 (global-set-key (kbd "<f5>") 'backward-sentence)
@@ -206,7 +206,8 @@ the entire buffer."
 
 (define-key global-map (kbd "<down>") 'end-of-defun)
 (define-key global-map (kbd "<up>") 'beginning-of-defun)
-(global-set-key (kbd "<insert>") 'keyboard-quit)
+(global-set-key (kbd "<insert>") 'keyboard-escape-quit)
+;; (global-set-key (kbd "<pause>") 'keyboard-escape-quit)
 
 ;; (define-key xah-fly-insert-map (kbd "\\") 'xah-fly-command-mode-activate)
 (global-set-key (kbd "\\") 'dabbrev-expand)
@@ -223,7 +224,7 @@ the entire buffer."
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(blink-cursor-mode nil)
- '(custom-enabled-themes '(leuven))
+ '(custom-enabled-themes '(wombat))
  '(tool-bar-mode nil)
  '(visible-cursor nil)
  '(xah-fly-keys t))
@@ -234,5 +235,5 @@ the entire buffer."
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
 (put 'narrow-to-region 'disabled nil
-)
+     )
 (put 'erase-buffer 'disabled nil)
--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -1849,11 +1849,11 @@ Version: 2017-01-17 2021-08-12"
 (defun xah-insert-ascii-single-quote () (interactive) (xah-insert-bracket-pair "'" "'") )
 (defun xah-insert-emacs-quote () (interactive) (xah-insert-bracket-pair "`" "'") )
 (defun xah-insert-corner-bracket () (interactive) (xah-insert-bracket-pair "「" "」" ) )
-(defun xah-insert-white-corner-bracket () (interactive) (xah-insert-bracket-pair "『" "』") )
+(defun xah-insert-white-corner-bracket () (interactive) (xah-insert-bracket-pair "{" "}") )
 (defun xah-insert-double-angle-bracket () (interactive) (xah-insert-bracket-pair "/" "/") )
 (defun xah-insert-white-lenticular-bracket () (interactive) (xah-insert-bracket-pair "〖" "〗") )
-(defun xah-insert-black-lenticular-bracket () (interactive) (xah-insert-bracket-pair "【" "】") )
-(defun xah-insert-tortoise-shell-bracket () (interactive) (xah-insert-bracket-pair "〔" "〕" ) )
+(defun xah-insert-black-lenticular-bracket () (interactive) (xah-insert-bracket-pair "{" "}") )
+(defun xah-insert-tortoise-shell-bracket () (interactive) (xah-insert-bracket-pair "+" "+" ) )
 
 (defun xah-insert-hyphen ()
   "Insert a HYPHEN-MINUS character."
